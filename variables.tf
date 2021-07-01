a226335a354b45a268ccb178a26332c2abd705b2
sivanagireddyb <sivanagireddy208@gmail.com>
2021-07-01 16:56:42 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "clusterae_tgs" {
+  type = list(string)
+  default = [""]
+}
+
+variable "clusterb_tgs" {
+  type = list(string)
+  default = [""]
+}
+
+variable "clusterc_tgs" {
+  type = list(string)
+  default = [""]
+}
+
+variable "clusterae_instance_ids" {
+ type = list(string)
+ default = [""]
+}
+
+variable "clusterb_instance_ids" {
+ type = list(string)
+ default = [""]
+}
+
+variable "clusterc_instance_ids" {
+ type = list(string)
+ default = [""]
+}
